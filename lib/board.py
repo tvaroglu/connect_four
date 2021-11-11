38642f04dd43cf244233120ab4e68c2a02f106aa
Taylor Varoglu <tava8398@colorado.edu>
2021-11-11 15:19:44 -0700

    Add tests for board_setup

--- lib/board.py
+++ lib/board.py
@@ -1,6 +1,17 @@
 class Board:
     def __init__(self):
         self.column_range = range(1, 8)
+        self.row_range = range(1, 8)
+
+    def column_labels(self):
+        output_list = []
+        for i in self.column_range:
+            output_list.append(f' {str(i)}  ')
+        return output_list
+
+    def full_column_label(self):
+        joined = ''.join(self.column_labels())
+        return f' {joined}'
 
     def row_dividers(self):
         output_list = []
@@ -13,12 +24,16 @@ class Board:
     def full_row_divider(self):
         return ''.join(self.row_dividers())
 
-    def column_labels(self):
-        output_list = []
-        for i in self.column_range:
-            output_list.append(f' {str(i)}  ')
-        return output_list
+    def board_setup(self):
+        board = [self.full_column_label()]
+        for row in self.row_range:
+            board.append(self.full_row_divider())
+        return board
 
-    def full_column_label(self):
-        joined = ''.join(self.column_labels())
-        return f' {joined}'
+    def print_board(self):
+        for row in self.board_setup():
+            print(row)
+
+
+# b = Board()
+# b.print_board()
--- test/test_board.py
+++ test/test_board.py
@@ -5,17 +5,6 @@ class TestBoard(unittest.TestCase):
     def setUp(self):
         self.board = Board()
 
-    def test_row_dividers(self):
-        dividers = self.board.row_dividers()
-        self.assertEqual(len(dividers), 7)
-        for d in dividers[0:5]:
-            self.assertEqual(d, '+---')
-        self.assertEqual(dividers[6], '+---+')
-
-    def test_full_row_divider(self):
-        full_row_divider = self.board.full_row_divider()
-        self.assertEqual(full_row_divider, '+---+---+---+---+---+---+---+')
-
     def test_column_labels(self):
         labels = self.board.column_labels()
         self.assertEqual(len(labels), 7)
@@ -27,6 +16,24 @@ class TestBoard(unittest.TestCase):
     def test_full_column_label(self):
         full_label = self.board.full_column_label()
         self.assertEqual(full_label, '  1   2   3   4   5   6   7  ')
+
+    def test_row_dividers(self):
+        dividers = self.board.row_dividers()
+        self.assertEqual(len(dividers), 7)
+        for d in dividers[0:5]:
+            self.assertEqual(d, '+---')
+        self.assertEqual(dividers[6], '+---+')
+
+    def test_full_row_divider(self):
+        full_row_divider = self.board.full_row_divider()
+        self.assertEqual(full_row_divider, '+---+---+---+---+---+---+---+')
+
+    def test_board_setup(self):
+        board = self.board.board_setup()
+        labels = self.board.full_column_label()
+        self.assertEqual(board[0], labels)
+        for row in board[1:-1]:
+            self.assertEqual(row, self.board.full_row_divider())
 
 
 if __name__ == '__main__':
