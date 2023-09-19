3cb3e1e68ff453a2864a4881cd13a7a6be1ffd73
Taylor Varoglu <taylor.varoglu@gmail.com>
2023-09-19 14:45:39 -0600

    Refactor player.py

--- lib/player.py
+++ lib/player.py
@@ -1,8 +1,8 @@
 class Player:
-    def __init__(self, name, color):
-        self.name = name
+
+    def __init__(self, color, name='Skynet'):
         self.color = color
-        self.default_name = 'Skynet'
+        self.name = name
 
     def full_color(self):
         return 'Red' if self.color == '🟥' else 'Black'
