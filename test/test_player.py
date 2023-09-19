34e7139c527ca576ba7c53116f4a03d9a01e1d91
Taylor Varoglu <taylor.varoglu@gmail.com>
2023-09-19 14:48:46 -0600

    Update test_player.py based on revised player.py module

--- test/test_player.py
+++ test/test_player.py
@@ -3,21 +3,23 @@ from lib.player import Player
 
 class TestPlayer(unittest.TestCase):
     def setUp(self):
-        self.red_piece = '🟥'
+        self.red_piece   = '🟥'
         self.black_piece = '⬛️'
-        self.player_1 = Player('John', self.red_piece)
-        self.player_2 = Player('Jane', self.black_piece)
+        self.player_1 = Player(self.red_piece, 'John')
+        self.player_2 = Player(self.black_piece, 'Jane')
+        self.default_player = Player(self.black_piece)
 
     def test_attributes(self):
         self.assertEqual(self.player_1.name, 'John')
         self.assertEqual(self.player_2.name, 'Jane')
         self.assertEqual(self.player_1.color, self.red_piece)
         self.assertEqual(self.player_2.color, self.black_piece)
-        self.assertEqual(self.player_1.default_name, 'Skynet')
+        self.assertEqual(self.default_player.name, 'Skynet')
 
     def test_full_color(self):
         self.assertEqual(self.player_1.full_color(), 'Red')
         self.assertEqual(self.player_2.full_color(), 'Black')
+        self.assertEqual(self.default_player.full_color(), 'Black')
 
 
 if __name__ == '__main__':
