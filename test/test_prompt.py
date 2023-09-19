d996bee85ee4772e1300ebf41902670b841eb500
Taylor Varoglu <taylor.varoglu@gmail.com>
2023-09-19 14:49:45 -0600

    Update test_prompt.py based on revised prompt.py module

--- test/test_prompt.py
+++ test/test_prompt.py
@@ -5,24 +5,38 @@ from lib.player import Player
 class TestPrompt(unittest.TestCase):
     def setUp(self):
         self.prompt = Prompt()
-        self.red_piece = '🟥'
+        self.red_piece   = '🟥'
         self.black_piece = '⬛️'
-        self.player_1 = Player('John', self.red_piece)
-        self.player_2 = Player('Jane', self.black_piece)
+        self.player_1 = Player(self.red_piece, 'John')
+        self.player_2 = Player(self.black_piece, 'Jane')
 
     def test_welcome(self):
-        self.assertEqual(self.prompt.welcome(), 'Welcome to ConnectFour!')
+        self.assertEqual(
+            self.prompt.welcome(), 'Welcome to ConnectFour!')
 
     def test_request_name(self):
-        self.assertEqual(self.prompt.request_name(), "What is your name?\n > ")
+        self.assertEqual(
+            self.prompt.request_name(), "What is your name?\n > ")
 
     def test_greet_player(self):
-        self.assertEqual(self.prompt.greet_player(self.player_1.name, self.player_1.full_color()), f"Welcome, {self.player_1.name}! Your color is '{self.player_1.full_color()}'")
-        self.assertEqual(self.prompt.greet_player(self.player_2.name, self.player_2.full_color()), f"Welcome, {self.player_2.name}! Your color is '{self.player_2.full_color()}'")
+        self.assertEqual(
+            self.prompt.greet_player(
+            self.player_1.name, self.player_1.full_color()),
+            f"Welcome, {self.player_1.name}! Your color is '{self.player_1.full_color()}'")
+        self.assertEqual(
+            self.prompt.greet_player(
+            self.player_2.name, self.player_2.full_color()),
+            f"Welcome, {self.player_2.name}! Your color is '{self.player_2.full_color()}'")
 
     def test_request_placement(self):
-        self.assertEqual(self.prompt.request_placement(self.player_1.name), f"Your turn, {self.player_1.name}. Please enter a number between 1 and 7 to place a piece into the board:\n > ")
-        self.assertEqual(self.prompt.request_placement(self.player_2.name), f"Your turn, {self.player_2.name}. Please enter a number between 1 and 7 to place a piece into the board:\n > ")
+        self.assertEqual(
+            self.prompt.request_placement(
+            self.player_1.name),
+            f"Your turn, {self.player_1.name}. Please enter a number between 1 and 7 to place a piece into the board:\n > ")
+        self.assertEqual(
+            self.prompt.request_placement(
+            self.player_2.name),
+            f"Your turn, {self.player_2.name}. Please enter a number between 1 and 7 to place a piece into the board:\n > ")
 
     def test_line_break(self):
         self.assertEqual(self.prompt.line_break(), "\n")
@@ -31,13 +45,17 @@ class TestPrompt(unittest.TestCase):
         self.assertEqual(self.prompt.start_game(), "Let's play!")
 
     def test_new_game(self):
-        self.assertEqual(self.prompt.new_game(), "Would you like to play again? (y/n)\n > ")
+        self.assertEqual(
+            self.prompt.new_game(), "Would you like to play again? (y/n)\n > ")
 
     def test_end_game(self):
-        self.assertEqual(self.prompt.end_game(), "Game exiting...\n Goodbye!")
+        self.assertEqual(
+            self.prompt.end_game(), "Game exiting...\n Goodbye!")
 
     def test_announce_victor(self):
-        self.assertEqual(self.prompt.announce_victor(self.player_1.full_color()), f"{self.player_1.full_color()} wins!!")
+        self.assertEqual(
+            self.prompt.announce_victor(
+            self.player_1.full_color()), f"{self.player_1.full_color()} wins!!")
 
     def test_sanitize_request(self):
         user_input = 'NO'
@@ -45,13 +63,26 @@ class TestPrompt(unittest.TestCase):
         self.assertEqual(self.prompt.default_request, 'y')
 
     def test_request_game_mode(self):
-        self.assertEqual(self.prompt.request_game_mode(), "Please select game mode\n((1) vs (2) player):\n > ")
+        self.assertEqual(
+            self.prompt.request_game_mode(),
+            "Please select game mode\n((1) vs (2) player):\n > ")
 
     def test_game_mode(self):
         self.assertEqual(self.prompt.game_mode(), f'Entering 1-player game mode')
         self.assertEqual(self.prompt.game_mode(2), f'Entering 2-player game mode')
         self.assertEqual(self.prompt.game_mode(1), f'Entering 1-player game mode')
         self.assertEqual(self.prompt.game_mode('dfsasfsf'), f'Entering 1-player game mode')
+
+    def test_invalid_placement(self):
+        self.assertEqual(self.prompt.invalid_placement(),
+                         "Sorry! Can't place a piece there, please try another move.")
+
+    def test_piece_placed(self):
+        self.assertEqual(self.prompt.piece_placed(), 'Nice move!')
+
+    def test_draw(self):
+        self.assertEqual(self.prompt.draw(),
+                         'Uh oh! No more slots open... game over!!')
 
 
 if __name__ == '__main__':
