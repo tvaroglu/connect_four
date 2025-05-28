60bd12322278acc88966acfdff638d64ca4de479
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-05-28 10:47:28 +0300

    Update test_player.py to reflect blue vs black pieces

--- test/test_player.py
+++ test/test_player.py
@@ -4,22 +4,22 @@ from lib.player import Player
 class TestPlayer(unittest.TestCase):
     def setUp(self):
         self.red_piece   = '🟥'
-        self.black_piece = '⬛️'
+        self.blue_piece = '🟦'
         self.player_1 = Player(self.red_piece, 'John')
-        self.player_2 = Player(self.black_piece, 'Sarah')
-        self.default_player = Player(self.black_piece)
+        self.player_2 = Player(self.blue_piece, 'Sarah')
+        self.default_player = Player(self.blue_piece)
 
     def test_attributes(self):
         self.assertEqual(self.player_1.name, 'John')
         self.assertEqual(self.player_2.name, 'Sarah')
         self.assertEqual(self.player_1.color, self.red_piece)
-        self.assertEqual(self.player_2.color, self.black_piece)
+        self.assertEqual(self.player_2.color, self.blue_piece)
         self.assertEqual(self.default_player.name, 'Skynet')
 
     def test_full_color(self):
         self.assertEqual(self.player_1.full_color(), 'Red')
-        self.assertEqual(self.player_2.full_color(), 'Black')
-        self.assertEqual(self.default_player.full_color(), 'Black')
+        self.assertEqual(self.player_2.full_color(), 'Blue')
+        self.assertEqual(self.default_player.full_color(), 'Blue')
 
 
 if __name__ == '__main__':
